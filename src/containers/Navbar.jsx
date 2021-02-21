90c0385ccd101e46fa444e5a1a5f13826c76c034
MacBookPro <matthewpalmer9@outlook.com>
2021-02-21 12:50:24 -0600

    Restructured the navbar and added a Cart link to the right end of the navbar. Also changed some wording around on the landing page and used useHistory hook to set redirects to the home page.

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -11,21 +11,37 @@ export default function Navbar() {
         history.push("/shop");
     };
 
+    const redirectToHome = e => {
+        e.preventDefault();
+        history.push("/");
+    }
+
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-pink fixed-top">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">R&amp;A Boutique</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <a onClick={redirectToHome} className="navbar-brand" href="/">R&amp;A Boutique</a>
+                    <button className="black-menu navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="white-icon navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <a onClick={redirectToShop} className="nav-link active" aria-current="page" href="">Shop</a>
-                        <a className="nav-link" href="#">About</a>
-                        <a className="nav-link" href="#">Special Deals</a>
-                        <a className="nav-link" href="#" tabIndex="-1" aria-disabled="true">Inventory</a>
-                    </div>
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex">
+                            <a className="nav-link" href="#">
+                                <i class="fas cart fa-shopping-cart"></i>
+                                Cart $0.00
+                            </a>
+                        </form>
                     </div>
                 </div>
             </nav>
--- src/containers/sections/landing/MainLandingAdv.jsx
+++ src/containers/sections/landing/MainLandingAdv.jsx
@@ -18,8 +18,8 @@ export default function MainLandingAdv() {
                         <img className="object-cover object-center rounded" alt="hero" src={kidsIMG} />
                     </div>
                     <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">BOGOHO (placeholder)</h1>
-                    <p className="mb-8 leading-relaxed">Tis the season! Lorem ipsum of all sizes. Limited inventory! Buy one, get one HALF OFF! Add to your cart and apply coupon code: BOGOHO</p>
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they're gone!</h1>
+                    <p className="mb-8 leading-relaxed">Sales are only for 48 hours. Don't miss out on the best deals!</p>
                     <div className="move-to-middle flex justify-center">
                         <button onClick={redirectToShop} className="inline-flex text-white bg-pink-300 border-0 py-2 px-6 focus:outline-none hover:bg-pink-400 rounded text-lg">Shop Now</button>
                     </div>
